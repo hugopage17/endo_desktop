901fed419738d22bb546b4f112b45da7adf66266
hugopage17 <43106346+hugopage17@users.noreply.github.com>
2019-01-04 18:20:59 +1300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const {app, BrowserWindow} = require('electron')
 let mainWindow
 
 function createWindow () {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 960, height: 760})
   mainWindow.loadFile('index.html')
   mainWindow.on('closed', function () {
     mainWindow = null
